f70e3039af378b25cb96e9e5beed26ee0814fb64
Shashank <pshashank1729@gmail.com>
2022-05-24 10:44:51 +0530

    commit

--- src/context/GlobalStateSimple.js
+++ src/context/GlobalStateSimple.js
@@ -1,11 +1,10 @@
 import { createContext, useState, useEffect } from 'react'
-// import AppReducer from './AppReducer'
 
 const transactionsArr = [
-    // { id: 1, text: 'Flower', amount: -20 },
-    // { id: 2, text: 'Salary', amount: 300 },
-    // { id: 3, text: 'Book', amount: -10 },
-    // { id: 4, text: 'Camera', amount: 150 }
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 }
 ]
 
 
@@ -18,7 +17,7 @@ export const GlobalContext = createContext()
 //Provider Components
 
 export const GlobalProvider = ({ children }) => {
-    const URL = "http://localhost:5000/transactions"
+    // const URL = "http://localhost:5000/transactions"
 
     const [transactions, setTransactions] = useState(transactionsArr)
     useEffect(() => {
@@ -26,21 +25,21 @@ export const GlobalProvider = ({ children }) => {
     }, [])
 
     const deleteTransaction = async (id) => {
-        await fetch(`${URL}/${id}`, { method: 'DELETE' })
+        // await fetch(`${URL}/${id}`, { method: 'DELETE' })
         const filteredTransactions = transactions.filter((transaction) => (transaction.id !== id))
         setTransactions(filteredTransactions)
     }
     const addTransactions = async (newTransaction) => {
-        const method = {
-            method: 'POST',
-            headers: {
-                "Content-Type": 'application/json'
-            },
-            body: JSON.stringify(newTransaction)
-        }
-        const response = await fetch(`${URL}?_sort=id&_order=asc`, method)
-        const data = await response.json()
-        setTransactions([data, ...transactions,])
+        // const method = {
+        //     method: 'POST',
+        //     headers: {
+        //         "Content-Type": 'application/json'
+        //     },
+        //     body: JSON.stringify(newTransaction)
+        // }
+        // const response = await fetch(`${URL}?_sort=id&_order=asc`, method)
+        // const data = await response.json()
+        setTransactions([newTransaction, ...transactions,])
     }
     const fetchFeedback = async () => {
         try {
